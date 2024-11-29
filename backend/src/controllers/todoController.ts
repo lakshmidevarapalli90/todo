cab138085db8aa71e3556e833fc2db19a3430a35
lakshmi.devarapalli <lakshmi.devarapalli@itv.com>
2024-11-29 14:19:51 +0000

    replace html codes with lib

--- backend/src/controllers/todoController.ts
+++ backend/src/controllers/todoController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import { getTodos, addTodo, updateTodo, deleteTodo, completeTodo } from "../models/todoModel";
+import { StatusCodes } from "http-status-codes";
 
 export const fetchTodos = async(req: Request, res: Response) => {
     try {
         const todos = await getTodos();
-        res.status(200).json(todos);
+        res.status(StatusCodes.OK).json(todos);
     } catch (err) {
-        res.status(500).json(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
 }
 
@@ -14,9 +15,9 @@ export const createTodo = async (req: Request, res: Response) => {
     const { title, completed } = req.body;
     try {
         const todo = await addTodo({title, completed});
-        res.status(201).json(todo);
+        res.status(StatusCodes.CREATED).json(todo);
     } catch (err) {
-        res.status(500).json(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
 }
 
@@ -25,9 +26,9 @@ export const editTodo = async (req:Request, res:Response) => {
     const {title, completed } = req.body;
     try{
         const todo = await updateTodo(Number(id), {title, completed});
-        res.status(200).json(todo);
+        res.status(StatusCodes.OK).json(todo);
     } catch(err) {
-        res.status(500).json(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
 }
 
@@ -37,10 +38,10 @@ export const toggleTodo = async (req: Request, res: Response) => {
 
     try {
         const todo = await completeTodo(Number(id), completed);
-        res.status(200).json(todo);
+        res.status(StatusCodes.OK).json(todo);
     } catch (err) {
         console.error('Error updating todo:', err);
-        res.status(500).json({ error: 'Failed to update todo' });
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to update todo' });
     }
 };
 
@@ -48,8 +49,8 @@ export const removeTodo = async (req: Request, res: Response) => {
     const {id} = req.params;
     try {
         await deleteTodo(Number(id));
-        res.status(200).json({message: 'Todo Deleted'});
+        res.status(StatusCodes.OK).json({message: 'Todo Deleted'});
     } catch (err) {
-        res.status(500).json(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
 }

b7f9e590ca3a274b5413f1eb965e954d205928aa
lakshmi.devarapalli <lakshmi.devarapalli@itv.com>
2025-01-09 12:57:03 +0000

    split the columns into two

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import AddTodo from './components/AddTodo';
 import { getTodos } from './services/todoService';
 import TodoItem from './components/TodoItem';
 import { Todo } from './model';
 import "./App.css"
 
-const App:React.FC = () => {
+const App: React.FC = () => {
     const [todo, setTodo] = useState<string>('');
     const [todos, setTodos] = useState<Todo[]>([]);
 
@@ -17,25 +17,44 @@ const App:React.FC = () => {
         fetchTodos();
     }, []);
 
-   const handleAdd = async (newTodo: Todo) => {
+    const handleAdd = async (newTodo: Todo) => {
         setTodos(prevTodos => [...prevTodos, newTodo]);
-   };
+    };
+
+    const todoTasks = todos.filter(todo => !todo.completed);
+    const completedTasks = todos.filter(todo => todo.completed)
 
     return (
         <div className="App">
-            <span className="heading">Todo Tasks</span>
-            <AddTodo title={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-            <div className="todos">
-                {todos.map(todo => (
-                    <TodoItem
-                        key={todo.id}
-                        todo={todo}
-                        todos={todos}
-                        setTodos={setTodos}
-                    />
-                ))}
+            <span className="heading">Todo Application</span>
+            <AddTodo title={todo} setTodo={setTodo} handleAdd={handleAdd} />
+            <div className="container">
+                <div className="column">
+                    <div className="column-title">Todo Tasks</div>
+                    {
+                        todoTasks.map(todo => (
+                            <TodoItem
+                                key={todo.id}
+                                todo={todo}
+                                todos={todos}
+                                setTodos={setTodos} />
+                        ))
+                    }
+                </div>
+                <div className='column'>
+                    <div className="column-title">Completed Tasks</div>
+                    {
+                        completedTasks.map(todo => (
+                            <TodoItem
+                                key={todo.id}
+                                todo={todo}
+                                todos={todos}
+                                setTodos={setTodos}
+                            />
+                        ))}
+                </div>
             </div>
-         </div>
+        </div>
     )
 }
 
